dd178878a2b5f26c68ad5886d00c67da55325e7b
Kelvin1010 <canigetyournumber101010@gmail.com>
2022-09-12 03:34:53 +0700

    change

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ export function Header() {
         <Image src="/logo.png" height="70px" />
       </Link>
       <Text fontSize="3xl" fontWeight="bold">
-        Nền tảng phân tích dữ liệu
+        Nền tảng phân tích dữ liệu Ok 
       </Text>
       <Button onClick={handleLogout} variant="outline" colorScheme="red">
         Đăng xuất
